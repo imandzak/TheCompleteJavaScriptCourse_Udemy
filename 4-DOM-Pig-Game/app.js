9a0e80fea92763cf3e3c673df47e7daab5f513b7
Iryna Mandzak <irgolub1@gmail.com>
2017-05-24 09:06:23 +0200

    Game

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -12,11 +12,117 @@ GAME RULES:
 /*var score1 = 0;
 var score2 = 0;
 */
-var scores, roundScore, activePlayer, dice;
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+var scores, roundScore, activePlayer, gamePlaying;
+
+ init();
 
 dice = Math.floor(Math.random() * 6) + 1;
+console.log(dice);
 
 
+
+document.querySelector(".btn-roll").addEventListener("click",function() {
+    if(gamePlaying) {
+       //1. Random number
+   var dice = Math.floor(Math.random() * 6) + 1;
+                         
+     //2.Display the result                     
+   var diceDOM = document.querySelector(".dice");
+   diceDOM.style.display = "block";
+   diceDOM.src = "dice-" + dice +".png";
+    
+    
+    ///3. Update the round score IF the rolled number was NOT a 1
+    
+    if (dice !== 1) {
+        //add score
+        roundScore += dice;
+        document.querySelector("#current-" + activePlayer).textContent = roundScore;
+    } else {
+        //next player
+       nextPlayer();
+    } 
+    }
+                                          
+});
+
+document.querySelector(".btn-hold").addEventListener("click", function() {
+    if (gamePlaying) {
+      //add current score to global score
+    scores[activePlayer] += roundScore;
+       
+    //update the UI
+    
+    document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer];
+    //Check if player won the game
+    if (scores[activePlayer] >= 20) {
+        document.querySelector("#name-" + activePlayer).textContent = "Winner!";
+        document.querySelector(".dice").style.display = "none";
+        document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
+        document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
+        gamePlaying = false;
+    } else {
+        //next player
+     nextPlayer();
+    }
+   }        
+});
+  
+   
+    
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        /*  same as 
+        if (activePlayer ===0) {
+            activePlayer = 1;
+        } else {
+            activePayer = 0;
+        }
+        */
+        roundScore = 0;
+        
+        document.getElementById("current-0").textContent = "0";
+        document.getElementById("current-1").textContent = "0";
+        
+        document.querySelector(".player-0-panel").classList.toggle("active");
+        document.querySelector(".player-1-panel").classList.toggle("active");
+        
+       /// document.querySelector(".player-0-panel").classList.remove("active");
+
+        ///document.querySelector(".player-1-panel").classList.add("active");
+        
+        
+        document.querySelector(".dice").style.display("none");
+    
+}
+
+document.querySelector(".btn-new").addEventListener("click",
+    init);
+
+
+function init() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+    gamePlaying = true;
+
+    
+    document.querySelector(".dice").style.display = "none";
+
+    document.getElementById("score-0").textContent = "0";
+    document.getElementById("score-1").textContent = "0";
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+    document.getElementById("name-0").textContent = "Player 1";
+    document.getElementById("name-1").textContent = "Player 2";
+    document.querySelector(".player-0-panel").classList.remove("winner");
+    document.querySelector(".player-1-panel").classList.remove("winner");
+    document.querySelector(".player-0-panel").classList.remove("active");
+    document.querySelector(".player-1-panel").classList.remove("active");
+    document.querySelector(".player-0-panel").classList.add("active");
+}
+
+///document.querySelector("#current-" + activePlayer).innerHTML = "<em>" + dice + "</em>";
+
+//var x = document.querySelector("#score-0").textContent;
+///console.log(x);
